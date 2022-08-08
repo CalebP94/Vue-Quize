56095d2ee71ad5bf906bec441393719bde27a1ef
CalebP94 <cbpennint@gmail.com>
2022-08-08 08:32:30 -0400

    first commit

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,5 +1,14 @@
 <template>
-  <pre>{{ JSON.stringify(questions, null, 2) }}</pre>
+  <!-- <pre>{{ JSON.stringify(questions, null, 2) }}</pre> -->
+  <div>
+      <form>
+            <li v-for="(data, index) in questionArr" :key='index'>
+              {{ questionArr }}
+            </li>
+      </form>
+      <button v-on:click="add">Next</button>
+      <button v-on:click="sub">Previous</button>
+  </div>
 </template>
 
 <script>
@@ -11,7 +20,44 @@ export default {
     }
   },
   data() {
-    return {};
-  }
+    return {
+      counter:0,
+      questionList: this.questions,
+      questionArr:[],
+      answers:[],
+    };
+  },
+  //access props
+  //create functions that interact with globals (this) & objects returned in data
+  methods:{
+    add() {
+      this.counter++;
+      console.log(this.counter);
+      if(this.counter > 4){
+        this.counter= 0
+      }
+    },
+    sub(){
+      this.counter--;
+      console.log(this.counter);
+      if(this.counter < 0){
+        this.counter=4
+      }
+    },
+    questionsBlah(){
+        //console.log(this.questionList.answers)
+        let quiz = this.questionList;
+        quiz.map((i) => questionArr.push(i.text))
+        const answers = quiz.map((i)=> console.log(i))
+    }
+  }, 
+  mounted(){
+    this.add();
+    this.sub();
+    //this.questionsBlah()
+  },
+  beforeUpdate(){
+      alert("beforeUpdated");
+  },
 };
 </script>
